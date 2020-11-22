f50fc8ef2161866eae5a49af9f7ecdf4baad7293
Quang Nguyen <vincentnguyen1312@gmail.com>
2020-11-21 21:02:10 -0800

    update routing from menu item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,8 +3,8 @@ import MenuItem from '../menu-item/menu-item.component'
 import './directory.styles.scss'
 
 class Directory extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
 
         this.state = {
              sections : [
@@ -47,8 +47,13 @@ class Directory extends React.Component {
     render(){
         return(
             <div className='directory-menu'>
-                {this.state.sections.map(({title, imageUrl, size, id, linkUrl}) => 
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />   
+                {/* {this.state.sections.map(({title, imageUrl, size, id, linkUrl}) => 
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />   
+                )} */}
+
+                {/* ES6 Syntax, spreading props */}
+                {this.state.sections.map(({id, ...otherSectionProps}) => 
+                    <MenuItem key={id} {...otherSectionProps}/>   
                 )}
             </div>
         )
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; //Higher order component -> Takes component as argument-> Returns modified component
+
 import './menu-item.styles.scss'
 
-const MenuItem = ({title, imageUrl, size}) => 
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => 
+    <div className={`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
         <div style={{backgroundImage:`url(${imageUrl})`}} className="background-image"></div>
         <div className='content'>
             <div className='title'>{title.toUpperCase()}</div>
@@ -11,4 +13,5 @@ const MenuItem = ({title, imageUrl, size}) =>
     </div>
 
 
-export default MenuItem;
+export default withRouter(MenuItem);
+//Returns MenuItem that has access to location, match, and history props from React Router
