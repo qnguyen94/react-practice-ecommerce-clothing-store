90f2033411aa15a531e718a0e40ebd1abe8b2660
Quang Nguyen <vincentnguyen1312@gmail.com>
2020-11-22 13:36:52 -0800

    Added header with logo, shop, contract

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import ShopPage from './pages/shop/shop.component'
 import HomePage from './pages/homepage/homepage.component'
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
+      <Header />
       {/* Switch renders components as soon as there's a match and render no more */}
       <Switch>
         <Route exact path="/" component={HomePage} />
