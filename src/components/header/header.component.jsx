3787f460793cb86a0b1af4721aa6baa515f66fef
Quang Nguyen <vincentnguyen1312@gmail.com>
2020-11-28 20:24:33 -0800

    cart Icon on header, cart dropdown component, cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,9 +7,12 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 
 import { auth } from '../../firebase/firebase.utils'
 
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropDown from '../cart-dropdown/cart-dropdown.component'
+
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className='logo-container' to="/">
             <Logo className="logo" />
@@ -24,15 +27,18 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className="option" to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        { hidden? null : <CartDropDown />}
     </div>
 )
 
 
 // 'state' here will be state passed in by redux store
 // local component props now receives 'currentUser' from store
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
