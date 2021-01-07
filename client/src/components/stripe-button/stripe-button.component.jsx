b9ac0ec7d2a2fedd551dabda2b8af570a1758a41
Quang Nguyen <vincentnguyen1312@gmail.com>
2021-01-06 23:36:42 -0800

    fix public key

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
+  const publishableKey = 'pk_test_51I13CKA9GT4KzdprgWB4dHYNL5ZTbp5Do04tx4m6ohyeuwn504ZtiJjsPI9AA2oaSgyaiJGhzFApTGGNDAStSXt100EyhBBiuP';
 
   const onToken = token => {
     axios({
