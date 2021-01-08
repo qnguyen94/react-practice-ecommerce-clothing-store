3cca72d4b18827c41b18eeb0b4dce7e62eacb0c0
Quang Nguyen <vincentnguyen1312@gmail.com>
2021-01-07 16:26:00 -0800

    updating code to PWA compliant, added service worker, https, manifest

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -18,6 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  // Add HTTPS for requests in backend server. Only in production.
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
@@ -28,6 +31,12 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
+});
+
+//Service Worker
+// Up 1 dir because current dir will be inside client/public
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
 });
 
 app.post('/payment', (req, res) => {
